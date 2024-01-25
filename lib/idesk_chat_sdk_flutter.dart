1074555bdce48b6fa08d937efd503d15e99fec85
rahatcw <rahat.codeware@gmail.com>
2024-01-25 18:33:36 +0600

    package basics done

--- lib/idesk_chat_sdk_flutter.dart
+++ lib/idesk_chat_sdk_flutter.dart
@@ -1,26 +1,32 @@
 library idesk_chat_sdk_flutter;
 
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class IdeskChatSdkFlutter {
-  static final WebViewController _controller = WebViewController()
-    ..setJavaScriptMode(JavaScriptMode.unrestricted)
-    ..setBackgroundColor(Colors.transparent)
-    ..enableZoom(false)
-    ..setNavigationDelegate(
-      NavigationDelegate(
-        onProgress: (int progress) {},
-        onPageStarted: (String url) {},
-        onPageFinished: (String url) {},
-        onWebResourceError: (WebResourceError error) {},
-      ),
-    )
-    ..loadFlutterAsset('packages/idesk_chat_sdk_flutter/assets/index.html');
-
+  static late InAppWebViewController _webViewController;
   static Widget showChat() {
-    return WebViewWidget(
-      controller: _controller,
+    return InAppWebView(
+      initialFile: "packages/idesk_chat_sdk_flutter/assets/index.html",
+      initialOptions: InAppWebViewGroupOptions(
+        crossPlatform: InAppWebViewOptions(
+          javaScriptEnabled: true,
+        ),
+        android: AndroidInAppWebViewOptions(
+          useHybridComposition: true,
+        ),
+      ),
+      onWebViewCreated: (InAppWebViewController controller) {
+        _webViewController = controller;
+        _webViewController.addJavaScriptHandler(
+            handlerName: 'buttonClicked',
+            callback: (args) {
+              print(args);
+            });
+      },
+      onConsoleMessage: (controller, consoleMessage) {
+        print(consoleMessage);
+      },
     );
   }
 }
